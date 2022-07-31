2737e96bcb9024628eea90a8993b4829c036b1a5
Mariusz <mariuszbia@yahoo.com>
2022-07-31 18:30:14 -0500

    Completed assignment.

---
+++ lib/song.rb
@@ -0,0 +1,43 @@
+class Song
+    attr_reader :name, :artist, :genre
+    @@count = 0
+    @@artists = []
+    @@genres = []
+    # @@songs
+
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@count += 1
+        @@artists << artist
+        @@genres << genre
+        # song = {
+        #     name: name,
+        #     artist: artist,
+        #     genre: genre
+        # }
+
+        # @@songs << song
+    end
+
+    def self.count
+        @@count
+    end
+
+    def self.artists
+        @@artists.uniq
+    end
+
+    def self.genres
+        @@genres.uniq
+    end
+
+    def self.genre_count
+        @@genres.tally
+    end
+
+    def self.artist_count
+        @@artists.tally
+    end
+end
